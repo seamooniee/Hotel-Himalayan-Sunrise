e7be4fd8fd0d69ba2bfcf0a40236ed511c95ffd5
Rijan Shrestha <seamoonique@gmail.com>
2026-03-24 18:22:58 +0545

    updated with some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
 
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
