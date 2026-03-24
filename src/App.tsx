18f9a8ac54194902c2f5c08369b54f8756f1a275
Rijan Shrestha <seamoonique@gmail.com>
2026-03-24 18:19:02 +0545

    Revert "updated with some changes"
    This reverts commit 9d6f47bae82da33bb84b511d6939ea318d4adb49.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Home from './pages/Home'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Router>
+
     </>
   )
 }
